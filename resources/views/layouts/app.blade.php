fd716656c8e28bfc3d8fffbab3ab75cb16a4bdac
vagrant <vagrant@homestead>
2021-02-12 12:20:33 +0000

    fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 <div id="app">
     <nav class="navbar navbar-expand-lg navbar-warning bg-warning">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">{{ env('APP_NAME') }}</a>
+            <a class="navbar-brand" href="/">{{ env('APP_NAME') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
